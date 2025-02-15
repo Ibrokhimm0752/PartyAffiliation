b1a4e48a933a13c6f09f099a31a797065f33f30a
Ibrokhimm0752 <Ibrokhimm0752@hstat.org>
2025-02-14 19:15:23 -0500

    updated the code

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
 public class PartyAffiliation {
     public static void main(String[] args) {
-        String party = "D";//Choose the symbol you want
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose your party affiliation:");
+        System.out.println("D - Democrat");
+        System.out.println("R - Republican");
+        System.out.println("I - Independent");
+        System.out.print("Enter your choice (D, R, I): ");
+        String input = scanner.nextLine().trim().toUpperCase();
 
-        if (party.equals("D")) {// Input is if you select D
-            System.out.println("You get a Democratic Donkey.");// output you get Democratic Donkey
-        } else if (party.equals("R")) {// Input is if you select R
-            System.out.println("You get a Republican Elephant.");// output you get Republican Elephant
-        } else if (party.equals("I")) {// Input is if you select I
-            System.out.println("You get an Independent Person.");// output you get Independent Person
-        } else {// Input is if you select any other letter
-            System.out.println("You get an Other.");//Output you get other
+        switch (input) {
+            case "D":
+                System.out.println("You get a Democratic Donkey.");
+                break;
+            case "R":
+                System.out.println("You get a Republican Elephant.");
+                break;
+            case "I":
+                System.out.println("You get an Independent Person.");
+                break;
+            default:
+                System.out.println("You get Other.");
+                break;
         }
+
+        if (input.length() == 1) {
+            if (input.equals("D") || input.equals("R") || input.equals("I")) {
+                System.out.println("Valid input detected.");
+            } else {
+                System.out.println("Invalid party option.");
+            }
+        }
+        scanner.close();
     }
 }
